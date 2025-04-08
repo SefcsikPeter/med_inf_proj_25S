01eb654b327cf9d6471c4331ee39463dd44323b7
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-04-08 15:28:38 +0200

    Added story service

--- app/frontend/infection-visualizer/src/app/component/story/story.component.ts
+++ app/frontend/infection-visualizer/src/app/component/story/story.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-story',
   imports: [],
+  standalone: true,
   templateUrl: './story.component.html',
   styleUrl: './story.component.css'
 })
