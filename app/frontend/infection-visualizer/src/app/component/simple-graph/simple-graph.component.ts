8d98d9fad40e960a9d14e980f9bec1625c50624c
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-04-08 15:21:17 +0200

    Added simple graph component

---
+++ app/frontend/infection-visualizer/src/app/component/simple-graph/simple-graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-graph',
+  imports: [],
+  templateUrl: './simple-graph.component.html',
+  styleUrl: './simple-graph.component.css'
+})
+export class SimpleGraphComponent {
+
+}
