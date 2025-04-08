99a6531174f39e8a48959bb020ac6c56151adf88
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-04-08 16:52:29 +0200

    Added radial tree component

---
+++ app/frontend/infection-visualizer/src/app/component/radial-tree/radial-tree.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radial-tree',
+  imports: [],
+  templateUrl: './radial-tree.component.html',
+  styleUrl: './radial-tree.component.css'
+})
+export class RadialTreeComponent {
+
+}
