736a75d4f47db48adfa43afb640d79ad4bbc81ac
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-04-08 14:27:16 +0200

    Added implementation for infection tree

--- app/frontend/infection-visualizer/src/app/component/infection-tree/infection-tree.component.ts
+++ app/frontend/infection-visualizer/src/app/component/infection-tree/infection-tree.component.ts
@@ -17,13 +17,14 @@ export class InfectionTreeComponent implements AfterViewInit, OnInit {
   constructor(private treeService: InfectionTreeService) {}
 
   ngAfterViewInit() {
-    this.createTree();
+    
   }
 
   ngOnInit(): void {
-    this.treeService.getInfectionTree(300).subscribe({
+    this.treeService.getInfectionTree(50).subscribe({
       next: (data) => {
         this.infectionTreeData = data;
+        this.createTree();
         console.log('Tree data:', data);
       },
       error: (err) => {
@@ -33,10 +34,38 @@ export class InfectionTreeComponent implements AfterViewInit, OnInit {
   }
 
   createTree() {
+    const nodes = this.infectionTreeData.nodes;
+    const links = this.infectionTreeData.links;
+    const nodeById = new Map(nodes.map((d: { id: any; }) => [d.id, { ...d, children: [] }]));
+    const add_labels = false;
+
+    links.forEach((link: any) => {
+      let sourceNode: any;
+      sourceNode = nodeById.get(link.source);
+      const targetNode = nodeById.get(link.target);
+      if (sourceNode && targetNode) {
+        sourceNode.children.push(targetNode)
+      }
+    });
+
+    const targetIds = new Set(links.map((d: { target: { id: any; }; }) => d.target.id || d.target));
+    const rootNode = nodes.find((d: { id: unknown; }) => !targetIds.has(d.id));
+    let root: any;
+    root = d3.hierarchy(nodeById.get(rootNode.id));
+    
+    const dx = 10;
+    const width = 1000;
+    let margin: any;
+    margin = { top: 10, right: 120, bottom: 10, left: 40 };
+    const dy = width / (1 + root.height);
+    const tree = d3.tree().nodeSize([dx, dy]);
+    const diagonal = d3.linkHorizontal().x((d: any) => d.y).y((d: any) => d.x);
+
     const svg = d3.select(this.treeContainer.nativeElement)
       .append('svg')
-      .attr('viewBox', [0, 0, 800, 600])
-      .style('font', '10px sans-serif');
+      .attr("viewBox", [-margin.left, -margin.top, width, dx])
+      .style("font", "10px sans-serif")
+      .style("user-select", "none");
 
     const gLink = svg.append("g")
       .attr("fill", "none")
@@ -47,6 +76,93 @@ export class InfectionTreeComponent implements AfterViewInit, OnInit {
     const gNode = svg.append("g")
       .attr("cursor", "pointer")
       .attr("pointer-events", "all");
+
+    root.x0 = 0;
+    root.y0 = 0;
+
+    root.descendants().forEach((d: { id: any; _children: any; children: any; }, i: any) => {
+      d.id = i;
+      d._children = d.children;
+    });
+
+    const update = (source: any) => {
+      const duration = 250;
+      const nodes = root.descendants().reverse();
+      const links = root.links();
+
+      tree(root);
+
+      let left = root;
+      let right = root;
+      root.eachBefore((node: any) => {
+        if (node.x < left.x) left = node;
+        if (node.x > right.x) right = node;
+      });
+
+      const height = right.x - left.x + margin.top + margin.bottom;
+      svg.transition().duration(duration)
+        .attr("viewBox", `${-margin.left} ${left.x - margin.top} ${width} ${height}`);
+
+      const node = gNode.selectAll<SVGGElement, any>("g")
+        .data(nodes, (d: any) => d.id);
+      
+      const nodeEnter = node.enter().append("g")
+        .attr("transform", (d: any) => `translate(${source.y0},${source.x0})`)
+        .attr("fill-opacity", 0)
+        .attr("stroke-opacity", 0)
+        .on("click", (event: any, d: any) => {
+          d.children = d.children ? null : d._children;
+          update(d);
+        });
+      
+      nodeEnter.append("circle")
+        .attr("r", 4)
+        .attr("fill", (d: any) => {
+          if (d === root) return "green";
+          switch (d.data.status) {
+            case "sev": return "orange";
+            case "crit": return "purple";
+            case "dead": return "red";
+            default: return "#999";
+          }
+        });
+      
+      node.merge(nodeEnter).transition().duration(duration) //maybe change
+        .attr("transform", (d: any) => `translate(${d.y},${d.x})`)
+        .attr("fill-opacity", 1)
+        .attr("stroke-opacity", 1);
+
+      node.exit().transition().duration(duration).remove()
+        .attr("transform", (d: any) => `translate(${source.y},${source.x})`)
+        .attr("fill-opacity", 0)
+        .attr("stroke-opacity", 0);
+      
+      const link = gLink.selectAll<SVGPathElement, any>("path")
+        .data(links, (d: any) => d.target.id);
+      
+      const linkEnter = link.enter().append("path")
+        .attr("d", (d: any) => {
+          const o = { x: source.x0, y: source.y0 };
+          return diagonal({ source: o, target: o } as any);
+        });
+      
+      link.merge(linkEnter).transition().duration(duration)
+        .attr("d", diagonal as any);
+      
+      link.exit().transition().duration(duration).remove()
+        .attr("d", (d: any) => {
+          const o = { x: source.x, y: source.y };
+          return diagonal({ source: o, target: o } as any);
+        });
+      
+      
+      root.eachBefore((d: any) => {
+          d.x0 = d.x;
+          d.y0 = d.y;
+        });
+    }
+
+    update(root);
   }
 
 }
