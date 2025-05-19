c549654d917677aca841190f489208ae2b8a87fb
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-05-19 18:52:02 +0200

    Started implementing mc component

--- app/frontend/infection-visualizer/src/app/component/mc-question/mc-question.component.ts
+++ app/frontend/infection-visualizer/src/app/component/mc-question/mc-question.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-mc-question',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgClass
+  ],
+  standalone: true,
   templateUrl: './mc-question.component.html',
   styleUrl: './mc-question.component.css'
 })
 export class McQuestionComponent {
+  @Input() question: {
+    text: string;
+    options: string[];
+    correctAnswer: string;
+  } = {text: 'Question not loaded correctly', options: ['1', '2', '3', '4'], correctAnswer: '2'};
 
+  selectedAnswer: string = '';
+  answerSubmitted = false;
+  isCorrect = false;
+
+  submitAnswer() {
+    this.answerSubmitted = true;
+    this.isCorrect = this.selectedAnswer === this.question.correctAnswer;
+  }
 }
