eed256ed7250dc9fe7bb6cfb4f70b7f84cedac50
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-08-11 10:34:14 +0200

    added badges overview

--- app/frontend/infection-visualizer/src/app/app.routes.ts
+++ app/frontend/infection-visualizer/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './component/home/home.component';
 import {StoryComponent} from './component/story/story.component';
 import {QuizComponent} from './component/quiz/quiz.component';
 import { SettingsComponent } from './component/settings/settings.component';
+import { BadgesComponent } from './component/badges/badges.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'story/:story_id', component: StoryComponent },
     { path: 'quiz/:story_id', component: QuizComponent },
     { path: 'settings', component: SettingsComponent },
+    { path: 'badges', component: BadgesComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
 ];
