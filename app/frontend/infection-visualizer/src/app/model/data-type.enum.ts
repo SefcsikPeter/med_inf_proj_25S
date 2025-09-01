1f1d3094051e43067583a2559d4a383dd517052d
Peter Sefcsik <e12025978@student.tuwien.ac.at>
2025-09-01 12:21:49 +0200

    adjusted to current options

--- app/frontend/infection-visualizer/src/app/model/data-type.enum.ts
+++ app/frontend/infection-visualizer/src/app/model/data-type.enum.ts
@@ -1,8 +1,8 @@
 export enum DataTypeEnum {
   dummy_tree = 'dummy_tree',
   example_tree = 'example_tree',
-  infection_tree = 'infection_tree',
   sir = 'sir',
   sir_at = 'sir_at',
-  coin_flip = 'coin_flip'
+  coin_flip = 'coin_flip',
+  coin_flip_gossip = 'coin_flip_gossip'
 }
